287df73e392e1c52a00817cd62dd3ba219585ea2
Jagannadha Srikar Uppada <srikarseven@gmail.com>
2020-02-13 14:08:45 +0530

    Update vars.tf

--- checks/sub/vars.tf
+++ checks/sub/vars.tf
@@ -1,11 +1,11 @@
 # Configure these variables
 
-variable "sample_var" {
+variable "var1" {
   description = "A sample_var to pass to the template."
   default     = "hello"
 }
 
-variable "sleepy_time" {
+variable "var2" {
   description = "How long our local-exec will take a nap."
   default     = 0
 }
